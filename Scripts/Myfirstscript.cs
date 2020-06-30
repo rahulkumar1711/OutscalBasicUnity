b1bf589b71704f11b5b43983816a61240aa129ca
rahulkumar1711 <60355624+rahulkumar1711@users.noreply.github.com>
2020-06-30 20:43:10 +0530

    Add files via upload

---
+++ Scripts/Myfirstscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Myfirstscript : MonoBehaviour
+{
+	public Button btn;
+	public string secondScene;
+    void Start()
+    {
+		btn.onClick.AddListener(()=>OpenSecondScene());
+		Debug.Log("First unity Script");
+    }
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.S))
+		{
+			Debug.Log("Print S");
+		}
+		else if (Input.GetKeyDown(KeyCode.F))
+		{
+			Debug.Log("Print F");
+		}
+		else if(Input.GetKeyUp(KeyCode.A))
+		{
+			Debug.Log("Print A");
+		}
+    }
+	public void OpenSecondScene()
+	{
+		SceneManager.LoadScene(secondScene);
+	}
+}
